bacbab98b14d7835feea2005035c196b6c121fd8
bhargavkanmalla <bhargavkanmalla@192.168.1.13>
2020-03-09 19:18:30 +0530

    First commit

---
+++ def/src/test/java/abc/TestOne.java
@@ -0,0 +1,19 @@
+package abc;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class TestOne 
+{
+	@Test
+	public void T1M1()
+	{
+		System.out.println("TestOne_MethodOne");
+	}
+	
+	@Test
+	public void T1M2()
+	{
+		System.out.println("TestOne_MethodTwo");
+	}
+}
---
+++ def/src/test/java/abc/TestTwo.java
@@ -0,0 +1,19 @@
+package abc;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class TestTwo 
+{
+	@Test
+	public void T2M1()
+	{
+		System.out.println("TestTwo_MethodOne");
+	}
+	
+	@Test
+	public void T2M2()
+	{
+		System.out.println("TestTwo_MethodTwo");
+	}
+}
